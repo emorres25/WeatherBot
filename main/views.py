97a77091de30ce5f8d5970013b60a3ded67bdc52
Shaurya <Shaurya@Shauryas-MBP.zyxel.com>
2016-09-07 18:59:49 +0530

    Changed temperature units

--- main/views.py
+++ main/views.py
@@ -37,7 +37,7 @@ def get_weather(fbid, lati,longi):
     r = requests.get(url)
     data = json.loads(r.text)
     place = data['sys']['country']
-    temp = data['main']['temp']
+    temp = float(data['main']['temp']) - 273.15
     wind = data['wind']['speed']
     #rain = data['rain']['3h']
     name = data['name']
